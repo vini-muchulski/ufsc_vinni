1cfb9cb6e30891b639af5ac3fdb50659aabd2854
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-05 17:57:41 -0300

    Add files via upload

---
+++ Untitled2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade;
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Sua idade eh %d \n", idade);
+
+
+    return 0;
+
+
+}
