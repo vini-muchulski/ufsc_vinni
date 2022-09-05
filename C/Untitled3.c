58c4e39e6ae88a72e072872a03d19e1f6d8fa6a9
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-05 18:00:45 -0300

    Add files via upload

---
+++ C/Untitled3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b, c;
+    printf("Digite dois numeros: \n");
+    scanf("%d %d", &a,&b);
+    c = a + b;
+    printf("%d + %d = %d",a ,b,c);
+
+    return 0;
+    }
+
