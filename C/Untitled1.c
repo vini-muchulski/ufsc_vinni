71542eb60523ca84e9ded70d28abd2f100b45b5a
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-10 15:45:50 -0300

    Add files via upload

--- C/Untitled1.c
+++ C/Untitled1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main(){
-printf("Ola mundo!\n");
-return 0;
-}
+ printf("Ola Mundo C \n");
+ return 0;
+ }
---
+++ C/multiplicacao .c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+int a,b,c;
+printf("Digite dois valores para multiplicacao: \n");
+scanf("%d %d", &a,&b);
+ c = a*b;
+ printf("%d * %d = %d",a,b,c);
+ return 0;
+ }
