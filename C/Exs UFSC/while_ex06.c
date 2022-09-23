13650550ea03fbf196bc0eca4a4bf984a21912fc
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-23 17:52:05 -0300

    Add files via upload

---
+++ C/Exs UFSC/while_ex06.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+int main(){
+    int i;
+
+printf("\n Incremento na condicao");
+i = 0;
+while (i++<10){
+printf("\n %i", i);
+}
+
+
+printf("\n Incremento na corpo");
+i = 1;
+while (i<10){
+
+printf("\n %i",    i);
+i++;
+}
+    return 0;
+}
