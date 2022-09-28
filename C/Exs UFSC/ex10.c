5848b8aad35859ab7ee41a1bd82ce8280ad32a8d
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-28 16:37:36 -0300

    Add files via upload

---
+++ C/Exs UFSC/ex10.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+int i;
+
+for (i=0;i<=30; i = i+3){
+    printf("%d \n",i);
+}
+return 0;
+}
---
+++ C/Exs UFSC/ex11.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+int i, soma;
+soma = 0;
+
+for (i = -10;i<=20;i =i + 5){
+ soma = soma + i;
+}
+
+printf("%d", soma);
+return 0;
+}
