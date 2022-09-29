7e1a8139c4d85190a1766e24bb42392665984d3c
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-28 22:06:32 -0300

    Add files via upload

--- C/Exs UFSC/ex15.c
+++ C/Exs UFSC/ex15.c
@@ -2,41 +2,39 @@
 
 int main(){
 
-int nota,somanotas,aprovados,reprovados,i,c;
-float media;
+int aprovados,reprovados,i,c;
+float media,somanotas,nota;
 aprovados = 0;
 reprovados = 0;
 
 for(i=1;i<=4;i++){
-        printf("Aluno %d \n", i);
+        printf("\n Aluno %d \n", i);
         for (c = 0; c <=3; c++){
     printf("Digite a nota: \n");
-    scanf("%d",&nota);
+    scanf("%f",&nota);
 
     somanotas = somanotas + nota;
 
     if (c==2){
-        media = somanotas/4;
+        media = somanotas/3;
+        printf("%.2f \n", media);
 
         if (media>=6){
             aprovados = aprovados +1;
-            printf("Aprovados mais 1");
+            printf("Aprovados mais 1 \n");
 
         }
         else{
             reprovados = reprovados +1;
-            printf("Reprovados mais 1");
+            printf("Reprovados mais 1 \n");
         }
-
+        somanotas = 0;
         break;
     }
-
-
         }
-
 }
 
-printf("Alunos aprovados: %d Alunos reprovados: %d", aprovados,reprovados);
+printf("Alunos aprovados: %d   -  Alunos reprovados: %d", aprovados,reprovados);
 
 return 0;
 }
