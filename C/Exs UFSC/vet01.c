805e9deb53ff8dbf38117b21bb5f69288b100749
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-09-30 17:20:01 -0300

    Add files via upload

---
+++ C/Exs UFSC/vet01.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+int v[10];
+int i;
+
+for (i = 0; i<10; i++){
+    v[i] = 2;
+
+}
+for(i=0;i<10;i++){
+printf("Indice: %i     Valor: %i \n", i , v[i]);
+
+}
+return 0;
+}
---
+++ C/Exs UFSC/vet02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+int l[14] = {22,14,16,12,11};
+int c;
+
+printf("ELEMENTO     VALOR \n");
+
+for(c=0;c<14;c++){
+printf(" %i              %i \n",c,l[c]);
+
+}
+return 0;
+}
