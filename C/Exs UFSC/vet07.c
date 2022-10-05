c08818a05fa5416defeb9f3ebf4a0d6d6acb0949
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-10-05 17:25:07 -0300

    Add files via upload

--- C/Exs UFSC/vet07.c
+++ C/Exs UFSC/vet07.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
-
+#define tam 25
 //remove espacos
 
 int main(){
-    int i;
-    char Vet[]= "OLA MUNDO e pessoas";
+    int i,j;
+
+    int tamanho_vet = tam;
+
+    char Vet[tam]= "sem espacos e vini";
 
 
-     for(i=0; i<15; i++){
+     for(i=0; i<tamanho_vet; i++){
         if(Vet[i]==' '){
             Vet[i]=Vet[i+1];
 
+            for (j=i;j<tamanho_vet;j++){
+            Vet[j]=Vet[j+1];
+            }
         }
     }
-    printf("String sem espacos em branco: \n %s \n", Vet );
+    printf("String sem espacos em branco: \n%s \n", Vet );
 
     return 0;
 }
