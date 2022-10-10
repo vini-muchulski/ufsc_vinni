3b3d95ade954f7280d39a17bc46e34eb3b7cac83
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-10-10 17:38:45 -0300

    Add files via upload

---
+++ C/Exs UFSC/auto avaliacao/questao01_02.c
@@ -0,0 +1,18 @@
+/*   Questao 1
+Variável
+Uma variavel é uma entidade que recebe um nome que tem por funcao identificá-la para uso dentro do programa e na memória do computador. Uma variável pode ser de vários tipos, como int para numeros inteiros, float para numeros de ponto flutuante e char para strings.
+
+Declaração e Inicialização de variáveis.
+Toda variável deve ser declarada antes do seu uso, pois assim o sistema já garante o espaço dela na memória do PC
+
+Operadores de atribuição e comparação
+Atribuição consiste em fornecer um valor a uma variável e é representado por um único sinal de =.
+Já a comparação de dois valores é feita por meio de dois sinais de iguais ==.
+
+ Comandos de seleção e repetição
+Como por exemplo, For, While e Switch, servem para repetir determinado código enquanto certa condição é verdadeira e iterar sobre ele.
+
+
+Questao 2
+ A - O algoritmo serve para realizar a soma dos algarismos de um dado número.
+*/
