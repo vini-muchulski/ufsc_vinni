c010059c2fe9b3afa4a368aa6266931a95f0b127
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-10-24 17:51:33 -0300

    Add files via upload

---
+++ C/Exs UFSC/potencia.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int potencia(int a, int n);
+
+int main(){
+
+int p
+
+p = potencia(5,2)
+
+return 0;
+}
+
+int potencia(int a, int n){
+n = n
+if (n==0){
+return 1;
+}
+else{
+return
+}
+
+
+}
