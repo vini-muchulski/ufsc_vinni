d2d98982d85527cbeaa0bcd34828e45aa37869ca
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-10-24 17:45:25 -0300

    Add files via upload

--- C/Exs UFSC/fibonacci_recurssivo.c
+++ C/Exs UFSC/fibonacci_recurssivo.c
@@ -3,14 +3,24 @@
 int fibonacci(int n);
 int main(){
 
-printf("%d \n",)
+int f;
 
-}
+f = fibonacci(6);
+
+printf("%d \n", f);
+
 
 return 0;
 }
 
 int fibonacci(int n){
+    if (n==1||n==2)
+{
+    return 1;
+}
+    else{
+    return fibonacci(n-1)+fibonacci(n-2);
+    }
 }
 
 
