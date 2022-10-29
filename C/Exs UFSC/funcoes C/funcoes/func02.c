cf47dfdda08cc4527c9f2ca660389052d518cc9b
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-10-29 18:05:58 -0300

    Add files via upload

---
+++ C/Exs UFSC/funcoes C/funcoes/func02.c
@@ -0,0 +1,26 @@
+// Escreva uma função int fatorial (int N) que calcula N! (fatorial de N), sendo que o valor inteiro (positivo) N é fornecido pelo usuário.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int fatorial( int n);
+
+int main(){
+    int n;
+
+    printf("Digite o valor: \n");
+    scanf("%d", &n);
+
+    printf("O fatorial de %d = %d", n, fatorial(n));
+
+    return 0;
+}
+
+int fatorial(int n){
+    int k,i;
+    k = 1;
+    for(i=n;i>1;i--){
+        k = k*i;
+    }
+    return k;
+}
