69e2c2cc0d70e249b67c37bd6d7cdfbd2502c342
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-11-09 19:52:18 -0300

    Add files via upload

--- C/Exs UFSC/funcoes C/funcoes/func03.c
+++ C/Exs UFSC/funcoes C/funcoes/func03.c
@@ -1,48 +1,48 @@
 //Escreva o programa que de forma repetida recebe pares de números inteiros  e mostra qual dos dois números é menor com auxílio da função
+// revisar - menor valor
 
 
 #include <stdio.h>
 #include <stdlib.h>
 
-int max_min(int n1,int n2);
+int max(int n1,int n2);
 
 int main(){
-    int n1,n2;
+    int n1;
+    int max_valor;
+    
     printf("Digite um valor: \n");
     scanf("%d", &n1);
 
-    printf("Digite outro valor: \n");
-    scanf("%d", &n2);
-    max_min(n1,n2);
+    max_valor = n1;
 
-    //printf("O maior valor foi %d ===== Menor valor %d", max.max_min(),max_min());
+    while(n1 != 0){
+
+    printf("Digite outro valor:[0 BREAK] \n");
+    scanf("%d", &n1);
+    max_valor =  max(n1,max_valor);
+    }
+
+    printf("o maior valor foi %d", max_valor);
 
     return 0;
 }
 
-int max_min(int n1,int n2){
+int max(int n1,int n2){
 
-    static int max;
-    static int min;
-
-    if(n1<n2 && n1<min){
-        min = n1;
-    }
-
-    if(n2<n1 && n2<min){
-        min = n2;
-    }
+    int max;
 
 
-    if(n2>n1 && n2>max){
+
+    if(n2>n1 ){
         max = n2;
     }
 
-    if(n1>n2 && n1>max){
+    if(n1>n2){
         max = n1;
     }
 
-    printf("O maior valor foi %d ===== Menor valor %d", max,min);
-    return max,min;
+    
+    return max;
 
 }
---
+++ C/Exs UFSC/funcoes C/funcoes/func05.c
@@ -0,0 +1,25 @@
+//Escreva uma função int reverse (int n) que recebe um número inteiro e retorna um número inteiro com a ordem dos dígitos invertida.
+
+#include <stdio.h>
+
+int reverse(int n);
+
+int main(){
+
+reverse(20);
+
+    return 0;
+}
+
+int reverse(int n){
+    int contador = 0;
+
+    //len caseiro
+
+    while(n/10>0){
+        contador++;
+    }
+
+    printf("contador %d", contador);
+
+}
