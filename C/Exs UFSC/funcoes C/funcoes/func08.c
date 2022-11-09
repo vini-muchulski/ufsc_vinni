290681d53390fa3b9371d6d91a11b36098672058
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-11-09 19:35:25 -0300

    Add files via upload

--- C/Exs UFSC/funcoes C/funcoes/func08.c
+++ C/Exs UFSC/funcoes C/funcoes/func08.c
@@ -1,9 +1,9 @@
 // jogo “Adivinhe o número”: jogador deve adivinhar um número gerado de forma aleatória
 
+
+// como usar a variavel tentativas da funcao dificuldade para se tornar o valor da variavel global tentativas
+
 #include <stdio.h>
-
-
-int compara_chute(int chute, int nrand);
 
 
 
@@ -11,8 +11,17 @@ int main(){
 
 int i;
 int chute = 0;
+int nivel;
+int dificuldades_tentativas;
+int aleat = aleatorio();
+int cont = 0;
 
-int alet = aleatorio();
+printf("Digite o nivel de dificuldade ---- \n1 = Facil - 8 tentativas \n2 = Medio - 5 tentativas \n3 = Dificil - 3 tentativas\n");
+scanf("%d",&nivel);
+dificuldades_tentativas = dificuldade(nivel);
+
+
+
 
 while (chute>=0){
     printf("Digite um valor para o chute [-1 Break]: \n");
@@ -22,9 +31,11 @@ while (chute>=0){
         break;
     }
     
-    if (compara_chute(chute,alet) == -1){
+    if (compara_chute(chute,aleat,dificuldades_tentativas-cont) == -1){
+        
         break;
     }
+    cont++;
 }
 
     return 0;
@@ -33,8 +44,25 @@ while (chute>=0){
 
 
 
+int dificuldade(int nivel){
+    int tentativas;
 
+    if(nivel ==1){
+          tentativas = 8;
+        
+    }
 
+    if(nivel ==2){
+          tentativas = 5;
+    }
+
+    if(nivel ==3){
+          tentativas = 3;
+    }
+    
+    return tentativas;
+    
+}
 
 
 int aleatorio(){
@@ -47,15 +75,22 @@ int aleatorio(){
     printf("nrand %d \n", nrand);
     return nrand;
 }
-int compara_chute(int chute, int nrand){
+int compara_chute(int chute, int nrand, int tentativas){
+        int chutes = tentativas;
     
-
     if( chute != nrand){
+        chutes--;
+        printf("Voce errou! Restam %i tentativas \n", chutes);
+        if (chutes>0){
         if(chute>nrand){
             printf("O valor chutado eh maior que o numero gerado \n");
         }
         else{
             printf("O chute eh menor que o valor gerado \n");
+        }}
+        else{
+            printf("Ficou sem tentativas =( FIM DE JOGO \n");
+            return -1;
         }
     }
     else{
