e87d53d78a6e507b78eb3ed5ae5e952a47bccf45
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-11-09 20:44:08 -0300

    Add files via upload

--- C/Exs UFSC/funcoes C/funcoes/func03.c
+++ C/Exs UFSC/funcoes C/funcoes/func03.c
@@ -7,23 +7,31 @@
 
 int max(int n1,int n2);
 
+int min(int n1,int n2);
+
 int main(){
     int n1;
     int max_valor;
+    int min_valor;
     
     printf("Digite um valor: \n");
     scanf("%d", &n1);
 
     max_valor = n1;
+    min_valor = n1;
 
     while(n1 != 0){
 
     printf("Digite outro valor:[0 BREAK] \n");
     scanf("%d", &n1);
+
+    if (n1 != 0){
     max_valor =  max(n1,max_valor);
+    min_valor = min(n1,min_valor);
+    }
     }
 
-    printf("o maior valor foi %d", max_valor);
+    printf("o maior valor foi %d \nmenor valor foi %d", max_valor, min_valor);
 
     return 0;
 }
@@ -44,5 +52,24 @@ int max(int n1,int n2){
 
     
     return max;
+
+}
+
+int min(int n1,int n2){
+
+    int min;
+
+
+
+    if(n2<n1 ){
+        min = n2;
+    }
+
+    if(n1<n2){
+        min = n1;
+    }
+
+    
+    return min;
 
 }
--- C/Exs UFSC/funcoes C/funcoes/func05.c
+++ C/Exs UFSC/funcoes C/funcoes/func05.c
@@ -1,25 +1,76 @@
 //Escreva uma função int reverse (int n) que recebe um número inteiro e retorna um número inteiro com a ordem dos dígitos invertida.
 
 #include <stdio.h>
+#include <string.h>
 
 int reverse(int n);
 
 int main(){
 
-reverse(20);
+int a;
+
+a = reverse(431);
+printf("numero %d", a);
+printf("\n---------------------------------\n");
+a = reverse(97631);
+printf("numero %d", a);
 
     return 0;
 }
 
 int reverse(int n){
-    int contador = 0;
+    int v[50];
+    int cont =0;
+    int algarismo;
+    int numero;
+    int i;
 
-    //len caseiro
+    int numero_return = 0;
+    int vet_exp[6];
 
-    while(n/10>0){
-        contador++;
+    int a,k;
+    a=1;
+    
+    // pega algarismo e gera vetor com eles em posicoes invertidas
+    while (n%10 !=0){
+        algarismo = n%10;
+        numero = n/10;
+        n = numero;
+
+        v[cont] = algarismo;
+
+        cont++;
+
+
+    }
+    // mostra numero inverso
+    for (i=0;i<cont;i++){
+        printf("%d ", v[i]);
+    }
+    printf("\ncont %d\n", cont);
+
+
+    // vetor para gerar a casa decimal do algarismo dps na hora da soma
+    for (i=0;i<cont;i++){
+        for(k=0;k<=i;k++){
+            a = a*10;
+
+        }
+        vet_exp[i] = a;
+        a=1;
     }
 
-    printf("contador %d", contador);
+    for (i=0;i<cont;i++){
+        printf("%d ", vet_exp[i]);
+    }
 
+    printf("\n");
+
+
+
+    for (i=0;i<cont;i++){
+        numero_return += v[i]*vet_exp[cont-i-1];
+    
+    }
+    return numero_return/10;
 }
