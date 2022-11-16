a0b73b460fbd294ff39f1bdf3d511d9cdf764ee7
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-11-16 17:44:31 -0300

    Add files via upload

---
+++ C/Exs UFSC/alocacao/ex_03.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int *vPtr;
+    int tamanho;
+    int i;
+
+    printf("Digite o tamanho: ");
+    scanf("%d", &tamanho);
+
+    //alocacao de memoria
+    vPtr = (int*)malloc(tamanho*sizeof(int));
+
+    if (vPtr == NULL){
+        printf("Erro de alocacao de memoria \n");
+    }
+
+    else
+    {
+        printf("\n Input dados: \n");
+        for(i=0;i<tamanho; i++){
+
+            printf("\n V[%d] = ", i);
+            scanf("%d", &vPtr[i]);
+
+        }
+
+        printf("\n printando: \n");
+        for(i=0;i<tamanho; i++){
+
+            printf(" V[%d] = %d \n",i,vPtr[i]);
+            
+        }
+    }
+
+    return 0;
+}
