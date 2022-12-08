3d40d3433cd57ae4ebe10f7e4509ba14c6eeba16
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2022-12-08 16:38:42 -0300

    Add files via upload

--- C/codigo padrao.c
+++ C/codigo padrao.c
@@ -8,3 +8,31 @@ int main(){
 
     return 0;
 }
+
+
+/*
+int **mat;
+int i,j;
+int qnt_pares =0;;
+int m,n;
+
+printf("Digite o numero de linhas: \n");
+scanf("%d", &n);
+
+
+printf("Digite o numero de colunas: \n");
+scanf("%d", &m);
+
+mat = (int**) malloc(n*sizeof(int*));
+
+// gera matriz
+for(i=0;i<n;i++){
+    mat[i] = (int*)malloc(m*sizeof(int));
+    for(j=0;j<m;j++){
+        
+        printf("Elemento [%d][%d] = \n",i,j);
+        scanf("%d", &mat[i][j] );
+ 
+    }
+}
+*/
