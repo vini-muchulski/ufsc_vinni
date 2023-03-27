02c86ef6842992a54b89c5ecca31ed37caf4dc8c
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-03-27 19:55:26 -0300

    Add files via upload

--- C++/pair.cpp
+++ C++/pair.cpp
@@ -15,6 +15,31 @@ using namespace std;
 
 int main(){
 
+vector <pair<string,int>> tabela;
+
+int idade;
+size_t i;
+string nome;
+
+
+for ( i = 0; i<3;i++){
+    cout << "Digite um nome: " << endl;
+    cin >> nome;
+
+     cout << "Digite uma idade: " << endl;
+     cin >> idade;
+
+     tabela.push_back(make_pair(nome,idade));
+
+
+}  
+
+for ( i = 0; i<tabela.size();i++){
+    cout << "nome  " << tabela[i].first<< " idade  " << tabela[i].second<< endl;
+
+
+} 
+
 
 
     return 0;
