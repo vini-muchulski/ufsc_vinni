3712ca79d41983ea550f169533f54d44822dc975
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-03-29 11:48:23 -0300

    Add files via upload

--- C++/arquivo_to_do.cpp
+++ C++/arquivo_to_do.cpp
@@ -1,10 +1,9 @@
-// contar todos os caracteres do texto
-
 #include <iostream>
 #include <string>
 #include <vector>
 #include <fstream>
 #include <utility>
+#include <algorithm>
 
 using namespace std;
 
@@ -12,14 +11,12 @@ using namespace std;
 bool Read_file(string path, string &conteudo , vector<string> &vetorzao);
 
 
-// std::  vector <pair<int,int>> tabela
-
-
-
-
-
-
 int main(){
+
+int i,j;
+string aux;
+
+int cont_aux =0;
 
 string filename("texto.txt");
 
@@ -28,14 +25,9 @@ string file_content = "";
 //vetor onde cada elemento é uma linha do texto.txt
 vector <string> vetorzao;
 
-int i,j;
-string aux;
-
-int cont_aux =0;
-
-
 // inicializa o vetor dos caracteres e atribui valor inicial 0 para a quantidade de caracteres encontrado
 vector <pair<string,int>>  caracteres;
+
 
 for (i=0;i<256;i++){
 
@@ -43,29 +35,6 @@ for (i=0;i<256;i++){
     caracteres.push_back(make_pair(aux,0));
     
 }
-/*for (i=0;i<256;i++){
-
-     cout << caracteres[i].first << "  " << caracteres[i].second <<  endl;
-    
-}*/
- // metodo count()
-
- for (i=0;i<vetorzao.size();i++){
-    
-
-    for (j=0;j<256;j++){
-    cont_aux = vetorzao[i].count(vetorzao[i].begin(), vetorzao[i].end(),caracteres[j].first);
-    
-
-
-    
-}
-
-    
-}
-
-
-
 
 
 
@@ -76,20 +45,38 @@ else{
     cout << "arquivo NAO lido" << endl;
 }
 
-//cout << file_content << endl;
-
 cout << vetorzao.size() << endl;
 
+/* for(i=0;i<vetorzao.size();i++){
+ cout << vetorzao[i] << endl;
 
+}
+*/
+
+
+
+ // metodo count()
+
+
+/* for (j=0;j<256;j++){
+    cont_aux = count(vetorzao[i].begin(), vetorzao[i].end(),caracteres[j].first);
+    caracteres[j].second = cont_aux;
+    //cout << cont_aux << endl;
+    cont_aux = 0; 
+} */
+
+
+
+for (i=0;i<256;i++){
+
+     cout << caracteres[i].first << "  " << caracteres[i].second <<  endl;
+    
+}
+
+cout << file_content << endl;
 
     return 0;
 }
-
-
-
-
-
-
 
 
 
