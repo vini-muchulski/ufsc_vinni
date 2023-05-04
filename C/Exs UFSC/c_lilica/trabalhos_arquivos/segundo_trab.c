63d3f7c3dee7d244361489e78346fc7904bdc14f
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-05-04 19:48:11 -0300

    Add files via upload

---
+++ C/Exs UFSC/c_lilica/trabalhos_arquivos/segundo_trab.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+
+int main() {
+    int linhas, i, j;
+    int opcao;
+
+    // Recebe o numero de linhas da forma
+    printf("Digite o numero de linhas da forma: ");
+    scanf("%d", &linhas);
+
+
+    printf("\n 1 - quadrado\n 2 - triangulo retangulo\n 3 - paralelogramo\n 4 - losango\n");
+    
+     // Pede a escolha  da forma geométrica a ser desenhada
+    printf("Digite a forma geometrica a ser desenhada: ");
+    scanf(" %d", &opcao);
+
+    switch (opcao) {
+        // Desenha a figura emitindo asteriscos durante cada iteração da primeira linha, apos isso ele trabalha com as excecoes para 
+        // Ver se deve emitir pontos no contorno da figura
+        case 1:
+            for (i = 1; i <= linhas; i++) {
+                for (j = 1; j <= linhas; j++) {
+                    if (i == 1 || i == linhas || j == 1 || j == linhas) {
+                        printf("* ");
+                    } else {
+                        printf("  ");
+                    }
+                }
+                printf("\n");
+            }
+            break;
+
+        case 2:
+        // Desenha um triângulo retângulo usando asteriscos seguindo o mesmo raciocinio
+            for (i = 1; i <= linhas; i++) {
+                for (j = 1; j <= i; j++) {
+                    if (j == 1 || j == i || i == linhas) {
+                        printf("* ");
+                    } else {
+                        printf("  ");
+                    }
+                }
+                printf("\n");
+            }
+            break;
+
+        case 3:
+         // Desenha um paralelogramo usando asteriscos
+            for (i = 1; i <= linhas; i++) {
+                for (j = 1; j <= linhas - i; j++) {
+                    printf("  ");
+                }
+                for (j = 1; j <= linhas; j++) {
+                    if (i == 1 || i == linhas || j == 1 || j == linhas) {
+                        printf("* ");
+                    } else {
+                        printf("  ");
+                    }
+                }
+                printf("\n");
+            }
+            break;
+
+        case 4:
+    // Desenha o losango usando os asteriscos
+    for (i = 1; i <= linhas/2 + 1; i++) {
+        for (j = 1; j <= linhas - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i*2-1; j++) {
+            if (j == 1 || j == i*2-1) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    for (i = linhas/2; i >= 1; i--) {
+        for (j = 1; j <= linhas - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i*2-1; j++) {
+            if (j == 1 || j == i*2-1) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    break;
+
+    }
+
+    return 0;
+}
