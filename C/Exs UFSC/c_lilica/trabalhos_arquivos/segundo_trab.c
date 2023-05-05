af3f193f1ca4ec5850375b89dad99e6eb10d3df2
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-05-05 19:15:40 -0300

    Add files via upload

--- C/Exs UFSC/c_lilica/trabalhos_arquivos/segundo_trab.c
+++ C/Exs UFSC/c_lilica/trabalhos_arquivos/segundo_trab.c
@@ -1,60 +1,74 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 
 int main() {
-    int linhas, i, j;
+    int linhas;
+    int i, j;
     int opcao;
 
-    // Recebe o numero de linhas da forma
+    // recebe o numero de linhas da forma
     printf("Digite o numero de linhas da forma: ");
     scanf("%d", &linhas);
 
 
     printf("\n 1 - quadrado\n 2 - triangulo retangulo\n 3 - paralelogramo\n 4 - losango\n");
-    
-     // Pede a escolha  da forma geométrica a ser desenhada
+     // pede a escolha  da forma geométrica a ser desenhada
     printf("Digite a forma geometrica a ser desenhada: ");
+    
     scanf(" %d", &opcao);
 
-    switch (opcao) {
-        // Desenha a figura emitindo asteriscos durante cada iteração da primeira linha, apos isso ele trabalha com as excecoes para 
-        // Ver se deve emitir pontos no contorno da figura
+    switch (opcao){
+        // desenha a figura emitindo asteriscos durante cada iteração da primeira linha, apos isso ele trabalha com as excecoes para 
+        // ver se deve emitir pontos no contorno da figura
         case 1:
             for (i = 1; i <= linhas; i++) {
-                for (j = 1; j <= linhas; j++) {
-                    if (i == 1 || i == linhas || j == 1 || j == linhas) {
+            for (j = 1; j <= linhas; j++) {
+            if (i == 1 || i == linhas || j == 1 || j == linhas) {
                         printf("* ");
-                    } else {
+                } 
+            else
+            {
                         printf("  ");
-                    }
-                }
-                printf("\n");
             }
+                }
+
+            printf("\n");
+            }
+
             break;
+
+
+
 
         case 2:
-        // Desenha um triângulo retângulo usando asteriscos seguindo o mesmo raciocinio
+        // desenha um triângulo retângulo usando asteriscos seguindo o mesmo raciocinio
             for (i = 1; i <= linhas; i++) {
-                for (j = 1; j <= i; j++) {
-                    if (j == 1 || j == i || i == linhas) {
-                        printf("* ");
-                    } else {
+            for (j = 1; j <= i; j++) {
+                if (j == 1 || j == i || i == linhas) {
+                    printf("* ");
+            } else{
                         printf("  ");
-                    }
-                }
-                printf("\n");
+            }
+            }
+            printf("\n");
             }
             break;
 
-        case 3:
-         // Desenha um paralelogramo usando asteriscos
+
+
+
+    case 3:
+         // desenha um paralelogramo usando asteriscos
             for (i = 1; i <= linhas; i++) {
-                for (j = 1; j <= linhas - i; j++) {
+            for (j = 1; j <= linhas - i; j++) {
                     printf("  ");
                 }
-                for (j = 1; j <= linhas; j++) {
-                    if (i == 1 || i == linhas || j == 1 || j == linhas) {
+            for (j = 1; j <= linhas; j++) {
+                if (i == 1 || i == linhas || j == 1 || j == linhas) {
                         printf("* ");
-                    } else {
+                } 
+                else{
                         printf("  ");
                     }
                 }
@@ -63,16 +77,16 @@ int main() {
             break;
 
         case 4:
-    // Desenha o losango usando os asteriscos
+    // desenha o losango usando os asteriscos
     for (i = 1; i <= linhas/2 + 1; i++) {
         for (j = 1; j <= linhas - i; j++) {
-            printf(" ");
+        printf(" ");
         }
         for (j = 1; j <= i*2-1; j++) {
             if (j == 1 || j == i*2-1) {
-                printf("*");
+            printf("*");
             } else {
-                printf(" ");
+            printf(" ");
             }
         }
         printf("\n");
@@ -81,10 +95,13 @@ int main() {
         for (j = 1; j <= linhas - i; j++) {
             printf(" ");
         }
+
+
         for (j = 1; j <= i*2-1; j++) {
             if (j == 1 || j == i*2-1) {
-                printf("*");
-            } else {
+            printf("*");
+            } 
+            else {
                 printf(" ");
             }
         }
@@ -92,7 +109,13 @@ int main() {
     }
     break;
 
+
+
+
     }
 
-    return 0;
+
+
+
+return 0;
 }
