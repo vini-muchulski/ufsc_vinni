c79bc7eb53df1cb8b87da42a89e04806e3097926
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-05-08 17:34:50 -0300

    Add files via upload

--- C++/exs herenca/ingresso_vip.cpp
+++ C++/exs herenca/ingresso_vip.cpp
@@ -56,6 +56,15 @@ class Ingresso_VIP : public Ingresso{
     set_preco(preco + valor_add);
 }
 
+    float get_preco_vip(){
+        return get_preco();
+    }
+
+    void set_preco_vip(float preco, float valor_extra){
+    valor_add = valor_extra;
+    set_preco(preco + valor_add);
+    }
+
 
 
 
@@ -69,7 +78,7 @@ int main(){
 
  Ingresso_VIP ing_vp = Ingresso_VIP(5,20,"Vingadores");
 
- cout<< "nome:  " << ing_vp.get_string() << "\npreco: " << ing_vp.get_preco() << endl; 
+ cout<< "nome:  " << ing_vp.get_string() << "\npreco: " << ing_vp.get_preco_vip() << endl; 
 
 
 
