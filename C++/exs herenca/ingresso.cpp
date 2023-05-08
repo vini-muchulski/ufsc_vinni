70b5340e34b07fc640667f7847fee7671bd01650
Vinícius <105316221+vini-muchulski@users.noreply.github.com>
2023-05-08 17:26:00 -0300

    Add files via upload

---
+++ C++/exs herenca/ingresso.cpp
@@ -0,0 +1,92 @@
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+
+using namespace std;
+
+
+
+class Ingresso{
+private:
+// atributos
+string filme;
+float valor;
+
+
+
+public:
+//metodos
+Ingresso(float preco, string nome){
+    valor = preco;
+    filme = nome;
+}
+
+ string preco_to_string(){
+    string preco = to_string(valor);
+    return preco;
+ }
+
+ float get_preco(){
+    return valor;
+ }
+
+    void set_preco(float novo_preco){
+    valor = novo_preco;
+ }
+
+ string get_string(){
+    return filme;
+     }
+void set_string(string descricao){
+    filme = descricao;
+}
+
+
+};
+
+
+
+int main(){
+
+vector <Ingresso> bilhetes;
+
+size_t i;
+size_t qntd;
+float preco;
+string nome_filme;
+float valor_total = 0;
+
+cout << "Quantos ingressos deseja? ";
+cin >> qntd;
+
+for(i=0;i<qntd;i++){
+    cout << "Digite o nome do filme" << endl;
+    cin >> nome_filme;
+
+    cout << "Digite o preco do ingresso" << endl;
+    cin >> preco;
+
+    Ingresso ingresso = Ingresso(preco, nome_filme);
+    bilhetes.push_back(ingresso);
+
+}
+
+cout << "-------------------" << endl;
+cout << "       TOTAL       " << endl;
+cout << "-------------------" << endl;
+
+for(i=0;i<bilhetes.size();i++){
+    cout << "Filme      "  << " Preco " <<  endl;
+    cout <<  bilhetes[i].get_string() << "      " << bilhetes[i].get_preco() << endl;
+    valor_total += bilhetes[i].get_preco();
+
+}
+cout << "-------------------" << endl;
+cout << "PRECO TOTAL = " << valor_total << endl; 
+
+
+
+    return 0;
+}
