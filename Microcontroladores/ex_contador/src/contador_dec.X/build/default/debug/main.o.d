9a463d525f29f585ea98dfaaf41f1baef2c705f2
Vini <105316221+vini-muchulski@users.noreply.github.com>
2023-12-08 13:33:44 -0300

    Delete Microcontroladores directory

--- Microcontroladores/ex_contador/src/contador_dec.X/build/default/debug/main.o.d
+++
@@ -1,3 +0,0 @@
-build/default/debug/main.o:  \
-	C:/codigos/ex_contador/src/contador_dec.X/main.asm  \
-
--- Microcontroladores/ex_display7/proteus/timer.X/build/default/production/timer_main.o.d
+++
@@ -1,3 +0,0 @@
-build/default/production/timer_main.o:  \
-	C:/codigos/ex_display7/proteus/timer.X/timer_main.asm  \
-
