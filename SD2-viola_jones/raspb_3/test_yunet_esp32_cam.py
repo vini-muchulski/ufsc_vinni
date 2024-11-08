8f63883f2d5a35235d3c1a95ddc175d2fbbd6db9
Vini <viniciusmuchulski2@gmail.com>
2024-11-08 14:07:05 -0300

    Update test_yunet_esp32_cam.py

--- SD2-viola_jones/raspb_3/test_yunet_esp32_cam.py
+++ SD2-viola_jones/raspb_3/test_yunet_esp32_cam.py
@@ -1,6 +1,6 @@
 import cv2 as cv
 
-model = 'face_detection_yunet_2023mar.onnx'
+model = 'face_detection_yunet_2022mar.onnx'
 input_size = (320, 320)  # Tamanho de entrada do modelo
 
 face_detector = cv.FaceDetectorYN.create(
@@ -8,21 +8,13 @@ face_detector = cv.FaceDetectorYN.create(
     top_k=5000, backend_id=cv.dnn.DNN_BACKEND_OPENCV, target_id=cv.dnn.DNN_TARGET_CPU
 )
 
-ip = 'http://192.168.0.107'
+ip = 'http://192.168.0.120'
 stream_url = f"{ip}:81/stream"
 cap = cv.VideoCapture(stream_url)
 
 if not cap.isOpened():
     print("Não foi possível conectar ao stream da ESP32-CAM")
     exit()
-
-ret, frame = cap.read()
-if not ret:
-    print("Falha na captura de vídeo")
-    exit()
-
-frame_height, frame_width = frame.shape[:2]
-face_detector.setInputSize((frame_width, frame_height))
 
 # Criar a janela antes do loop
 cv.namedWindow('Deteccao_Facial', cv.WINDOW_NORMAL)
@@ -32,12 +24,26 @@ while True:
     if not ret:
         print("Falha na captura de vídeo")
         break
- 
-    faces = face_detector.detect(frame)
+
+    # Redimensionar o quadro para o tamanho de entrada do modelo
+    resized_frame = cv.resize(frame, input_size)
+
+    # Detectar faces no quadro redimensionado
+    faces = face_detector.detect(resized_frame)
+
+    # Calcular as escalas de redimensionamento
+    scale_x = frame.shape[1] / input_size[0]
+    scale_y = frame.shape[0] / input_size[1]
 
     if faces[1] is not None:
         for face in faces[1]:
-            x, y, w, h = face[:4].astype(int)
+            x, y, w, h = face[:4]
+
+            # Ajustar as coordenadas
+            x = int(x * scale_x)
+            y = int(y * scale_y)
+            w = int(w * scale_x)
+            h = int(h * scale_y)
             center_x = int(x + w / 2)
             center_y = int(y + h / 2)
 
@@ -45,7 +51,10 @@ while True:
             cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
             # Desenhar os pontos de referência faciais
-            landmarks = face[4:14].reshape((5, 2)).astype(int)
+            landmarks = face[4:14].reshape((5, 2))
+            landmarks[:, 0] *= scale_x
+            landmarks[:, 1] *= scale_y
+            landmarks = landmarks.astype(int)
             for (lx, ly) in landmarks:
                 cv.circle(frame, (lx, ly), 2, (0, 0, 255), -1)
 
